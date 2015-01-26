56a16ddee3c3e85d970b34ebff811a1fb79029d5
Kazunari Kaizu <kwaizu@gmail.com>
2015-01-26 22:32:25 +0900

    Read multiple inputs and generate figures

--- plot.py
+++ plot.py
@@ -5,15 +5,21 @@ import sys
 import matplotlib.pylab as plt
 
 if len(sys.argv) > 1:
-    filename = sys.argv[1]
+    filenames = sys.argv[1: ]
 else:
-    filename = "result.txt"
-data = numpy.loadtxt(filename)
-Nsq = len(data)
-N = numpy.sqrt(Nsq)
-print N
-print data.max(), data.sum()
-# plt.imshow(data.reshape((N, N)), cmap=plt.cm.gray, interpolation='none')
-plt.imshow(data.reshape((N, N)), interpolation='none')
-plt.colorbar()
-plt.show()
+    filenames = ["result.txt"]
+
+for filename in filenames:
+    data = numpy.loadtxt(filename)
+    Nsq = len(data)
+    N = numpy.sqrt(Nsq)
+    print N
+    print data.max(), data.sum()
+    plt.imshow(data.reshape((N, N)), cmap=plt.cm.gray, interpolation='none')
+    # plt.imshow(data.reshape((N, N)), interpolation='none')
+    # plt.xlim(270, 330)
+    # plt.ylim(270, 330)
+    plt.colorbar()
+    # plt.savefig("%s.png" % filename)
+    plt.show()
+    plt.clf()
