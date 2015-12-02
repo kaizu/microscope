b90402f5c91f059191c9447ee18e7d3f80cfd474
Kazunari Kaizu <kwaizu@gmail.com>
2015-12-03 02:30:33 +0900

    Adapt a script to Python3

--- plot.py
+++ plot.py
@@ -13,13 +13,19 @@ for filename in filenames:
     data = numpy.loadtxt(filename)
     Nsq = len(data)
     N = numpy.sqrt(Nsq)
-    print N
-    print data.max(), data.sum()
+    print(N)
+    print(data.max(), data.sum())
+
+    # num_bins = 50
+    # n, bins, patches = plt.hist(data, num_bins, facecolor='green')
+
     # plt.imshow(data.reshape((N, N)), cmap=plt.cm.gray, interpolation='none')
+    # plt.imshow(data.reshape((N, N)), interpolation='none', clim=(0, 64000))
     plt.imshow(data.reshape((N, N)), interpolation='none')
     # plt.xlim(270, 330)
     # plt.ylim(270, 330)
     plt.colorbar()
+
     plt.savefig("%s.png" % filename)
     plt.show()
     plt.clf()
