4e37746f49379917b9fa36c5f0fd4d13c694b8fc
Kazunari Kaizu <kwaizu@gmail.com>
2015-12-04 13:02:12 +0900

    Move Python-related files

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ setup(
     name="microscope",
     cmdclass={"build_ext": build_ext},
     ext_modules=[
-        Extension("microscope", ["microscope.pyx"], language="c++",
+        Extension("microscope", ["python/microscope.pyx"], language="c++",
+                  depends=[
+                      "born_wolf_psf.hpp", "born_wolf_psf_table.hpp",
+                      "detection.hpp", "emission.hpp", "integration.hpp",
+                      "microscope.hpp", "point_spreading_functions.hpp"],
                   libraries=["gsl", "gslcblas"],
-                  include_dirs=[numpy.get_include()])
+                  include_dirs=[".", numpy.get_include()])
         ]
 )
