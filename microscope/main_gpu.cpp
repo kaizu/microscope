317d2247c799a5a1d47c20b2cc23aef2d313595c
Kazunari Kaizu <kaizu@vt001.localdomain>
2018-03-29 14:44:00 +0900

    Do the same thing with main

--- microscope/main_gpu.cpp
+++ microscope/main_gpu.cpp
@@ -136,6 +136,36 @@ std::string modify_filename(
     return outname;
 }
 
+void generate_random_points(
+    double points[][3], double intensity[], unsigned int const N_point, double const L)
+{
+    assert(N_point >= 200);
+
+    gsl_rng_env_setup();
+    const gsl_rng_type * T = gsl_rng_default;
+    gsl_rng * r = gsl_rng_alloc(T);
+
+    for (unsigned int i(0); i < N_point - 200; ++i)
+    {
+        points[i][0] = L * (0.5 - gsl_rng_uniform(r));
+        points[i][1] = L * (0.5 - gsl_rng_uniform(r));
+        points[i][2] = gsl_rng_uniform(r) * 3000;
+        // intensity[i] = emission(points[i][2]);
+        intensity[i] = 1.0;
+    }
+
+    for (unsigned int i(N_point - 200); i < N_point; ++i)
+    {
+        points[i][0] = L * (0.5 - gsl_rng_uniform(r));
+        points[i][1] = L * (0.5 - gsl_rng_uniform(r));
+        points[i][2] = 0.0;
+        // intensity[i] = emission(points[i][2]);
+        intensity[i] = 1.0;
+    }
+
+    gsl_rng_free(r);
+}
+
 bool save_data(char const filename[], double data[], unsigned int const data_size)
 {
     std::ofstream fout;
@@ -156,16 +186,18 @@ bool save_data(char const filename[], double data[], unsigned int const data_siz
 
 int main(int argc, char** argv)
 {
-    const unsigned int N_point(1000);
+    const unsigned int N_point(2620);
+    // const unsigned int N_point(1000);
     double points[N_point][3];
 
-    // const double wave_length = 508.0;
-    // const double N_A = 1.4;
-    // const double k = 2 * M_PI / wave_length;
-    // const double cutoff = 2000.0;
-    // const unsigned int N_pixel = 600;
-    // const double objective = 100.0;
-    // const double pixel_length = 6500 / objective;
+    const double wave_length = 508.0;
+    const double N_A = 1.4;
+    const double k = 2 * M_PI / wave_length;
+    const double cutoff = 2000.0;
+    const unsigned int N_pixel = 600;
+    const double objective = 100.0;
+    const double pixel_length = 6500 / objective;
+    const double L = pixel_length * N_pixel;
     // const double angle = 75.0;
     // const double flux_density = 20.0;
     // const std::pair<double, double> field
@@ -178,58 +210,80 @@ int main(int argc, char** argv)
     // const double exposure_time = 30e-3;
 
     // const double alpha = N_A * k;
-    // double focal_point[] = {0, 0, 0};
-    // // double focal_point[] = {0, 0, -240.0};
-
-    const double wave_length = 512.0;
-    const double N_A = 1.49;
-    const double k = 2 * M_PI / wave_length;
-    const double cutoff = 2000.0;
-    const unsigned int N_pixel = 512;
-    const double objective = 60.0 * 1.5 * 4.0;
-    // const double pixel_length = 16000 / objective;
-    const double pixel_length = 44.0;
-    const double angle = 75.0;
-    const double flux_density = 20.0;
-    const std::pair<double, double> field
-        = tirf_evanescent_field(angle, wave_length, flux_density);
-    const double ATsq = field.first;
-    const double d = field.second;
-    const double epsilon = 0.5 / 300;
-    // const double epsilon = 1.5 * 2.0 / 300;
-    // const double epsilon = 0.3;
-    // const double epsilon = 0.0145;
-    // const double epsilon = 0.00435;
-    const double absorption_cross_section = 3.19e-18;
-    const double exposure_time = 30e-3;
-
-    const double alpha = N_A * k;
     double focal_point[] = {0, 0, 0};
-    // double focal_point[] = {0, 0, -5 * pixel_length};
     // double focal_point[] = {0, 0, -240.0};
+
+    // const double wave_length = 512.0;
+    // const double N_A = 1.49;
+    // const double k = 2 * M_PI / wave_length;
+    // const double cutoff = 2000.0;
+    // const unsigned int N_pixel = 512;
+    // const double objective = 60.0 * 1.5 * 4.0;
+    // // const double pixel_length = 16000 / objective;
+    // const double pixel_length = 44.0;
+    // const double angle = 75.0;
+    // const double flux_density = 20.0;
+    // const std::pair<double, double> field
+    //     = tirf_evanescent_field(angle, wave_length, flux_density);
+    // const double ATsq = field.first;
+    // const double d = field.second;
+    // const double epsilon = 0.5 / 300;
+    // // const double epsilon = 1.5 * 2.0 / 300;
+    // // const double epsilon = 0.3;
+    // // const double epsilon = 0.0145;
+    // // const double epsilon = 0.00435;
+    // const double absorption_cross_section = 3.19e-18;
+    // const double exposure_time = 30e-3;
+
+    // const double alpha = N_A * k;
+    // double focal_point[] = {0, 0, 0};
+    // // double focal_point[] = {0, 0, -5 * pixel_length};
+    // // double focal_point[] = {0, 0, -240.0};
 
     boost::array<double, N_pixel * N_pixel> data;
     boost::array<double, N_point> intensity;
 
-    for (unsigned int idx = 1; idx < argc; ++idx)
+    if (argc == 1)
     {
-        const std::string filename(argv[idx]);
-        read_input(filename.c_str(), points, intensity.data(), N_point);
+        generate_random_points(points, intensity.data(), N_point, L);
 
-        // intensity.fill(1.0);
-        emission(
-            points, intensity.data(), N_point,
-            ATsq, d, epsilon, absorption_cross_section, exposure_time);
+        // emission(
+        //     points, intensity.data(), N_point,
+        //     ATsq, d, epsilon, absorption_cross_section, exposure_time);
+        emission(points, intensity.data(), N_point);
 
         data.fill(0.0);
         born_wolf_psf_tbl_gpu(
             data.data(), N_pixel, pixel_length, points, N_point,
             intensity.data(), focal_point, k, N_A, cutoff);
 
-        emccd_detection(data.data(), data.data(), data.size());
+        cmos_detection(data.data(), data.data(), data.size());
+        // emccd_detection(data.data(), data.data(), data.size());
+        save_data("result.txt", data.data(), data.size());
+    }
+    else
+    {
+        for (unsigned int idx = 1; idx < argc; ++idx)
+        {
+            const std::string filename(argv[idx]);
+            read_input(filename.c_str(), points, intensity.data(), N_point);
 
-        save_data(modify_filename(filename, "setting4_", ".dat").c_str(), data.data(), data.size());
-        // save_data(modify_filename(filename, "setting2_", ".dat").c_str(), data.data(), data.size());
+            // intensity.fill(1.0);
+            // emission(
+            //     points, intensity.data(), N_point,
+            //     ATsq, d, epsilon, absorption_cross_section, exposure_time);
+            emission(points, intensity.data(), N_point);
+
+            data.fill(0.0);
+            born_wolf_psf_tbl_gpu(
+                data.data(), N_pixel, pixel_length, points, N_point,
+                intensity.data(), focal_point, k, N_A, cutoff);
+
+            emccd_detection(data.data(), data.data(), data.size());
+
+            save_data(modify_filename(filename, "setting4_", ".dat").c_str(), data.data(), data.size());
+            // save_data(modify_filename(filename, "setting2_", ".dat").c_str(), data.data(), data.size());
+        }
     }
 
     return 0;
